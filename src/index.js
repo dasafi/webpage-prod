8ed317b238408dd99aef51cf3272de173b4a0307
dasafi <david_safi@hotmail.com>
2018-08-10 16:50:33 -0500

    segunda version

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Media from "./playlist/components/media.js";
 
-ReactDOM.render(<Media />, document.getElementById("app"));
-////////////////
+ReactDOM.render(<Media type = "video" title= "¿Quienes somos?" author= "D@ZS" image="./images/covers/logo_Prodaing.jpg" />, document.getElementById("app"));
+ 
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './media.css';
+import PropTypes from 'prop-types';
 
 class Media extends Component {
     render(){
@@ -12,15 +13,16 @@ class Media extends Component {
         }
         return (
             <div className="Media">
-                <div className = "Media-cover">
+                <div className = "Media-cover" onClick={this.handleClick}>
                     <img 
-                     src="./images/covers/logo_Prodaing.jpg"
+                     src={this.props.image}
                      alt=""
                      width={260}
                      height={160}
                     />
-                    <h3 className = "Media-title"> Bienvenido a Prodaing Ltda</h3>
-                    <p className = "Media-author">David Safi</p>
+                    
+                    <h3 className = "Media-title"> {this.props.title}</h3>
+                    <p className = "Media-author">{this.props.author}</p>
                 </div>           
                      
             </div>
@@ -28,6 +30,12 @@ class Media extends Component {
              
         )
     }
+}
+Media.propTypes = {
+    image: PropTypes.string,
+    title: PropTypes.string,
+    author: PropTypes.string,  
+    type: PropTypes.oneOf(['video', 'audio']),  
 }
 
 export default Media;
